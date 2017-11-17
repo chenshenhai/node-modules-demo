0297f9b0a35706def021e3dfd9eef0c5cfef5054
chenshenhai <cshenhai@gmail.com>
2017-11-17 23:50:19 +0800

    update demo4

--- demo4/index.js
+++ demo4/index.js
@@ -1,9 +1,14 @@
 import Koa from 'koa';
+import { render } from './lib/render.js';
 
 let app = new Koa();
 app.use((ctx, next) => {
-    ctx.body = 'my koa';
+    let view = ctx.url.substr(1);
+    ctx.body = render(view);
 })
 app.listen(3000, ()=>{
     console.log('the modules test server is starting');
 })
+
+
+ 
